61b3eabceac52bbfefddad3bdcf352b1c12946cb
Matthias Adler <macedigital@users.noreply.github.com>
2025-05-30 19:59:03 +0200

    conf: adjust lazyvim default settings
    - Remove "habamax" theme
    - Disable plugin update checks
    - Re-enable luarocks support
    - Disable hererocks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,13 +30,14 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
+    enabled = false, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
   rocks = {
-    enabled = false, -- Disables Luarocks completely
+    enabled = true, -- Disables Luarocks completely
+    hererocks = false, -- Disable here
   },
   performance = {
     rtp = {
