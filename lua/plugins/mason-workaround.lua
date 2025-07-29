13940b88c34e48e00680dc2625064be22717158a
Matthias Adler <macedigital@users.noreply.github.com>
2025-07-30 00:21:25 +0200

    fix: pin mason to v1
    For the time being prevent upgrade to version 2

---
+++ lua/plugins/mason-workaround.lua
@@ -0,0 +1,6 @@
+-- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+-- Workaround Mason 2.0 incompatibility issue
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
